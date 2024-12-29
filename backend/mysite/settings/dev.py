a1027c8f9fcf1728d22b51b090600f116c95962f
hash817 <wangyiran817@gmail.com>
2024-12-29 23:52:32 +0800

    Initial commit

---
+++ backend/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-r6lmm30$fy=y2hbh%#(c)7(zy7__ixbu$g2_)d-gs@0lz029$y"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ frontend-app/app/routes/topics.tsx
@@ -0,0 +1,5 @@
+import { Link } from "@remix-run/react";
+
+export default function aaa() {
+  return <Link to="/page1">Go to Pfdage 1</Link>;
+}
